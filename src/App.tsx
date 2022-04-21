35fa4496f9f5804f5f227986c0a7bb604c6a7e4a
evanzyli <45064281+edisonLzy@users.noreply.github.com>
2022-04-21 22:15:21 +0800

    feat: react-refress cause useEffect reinvoked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,41 +12,29 @@ function Child() {
 }
 
 function App() {
-  debugger;
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('mount');
+    return () => {
+      console.log('unmount');
+
+    }
+  }, [])
+
+  useMemo(() => {
+    console.log('memo run ');
+    return 1
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite!!</p>
         <input placeholder="请输入" />
         <Child />
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+            {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
       </header>
     </div>
